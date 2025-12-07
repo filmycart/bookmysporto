747f67825e9ff20a9140e3e743bd7b05f8b81c3c
filmycartin <filmycartin@gmail.com>
2025-12-07 15:24:49 +0530

    otp commit

--- api/user/validate_otp.php
+++ api/user/validate_otp.php
@@ -4,6 +4,13 @@
     $errors = new Errors();
     $send_mail = false;
 
+    $pgUserId = "";
+    if((isset($_POST["user_id"])) && (!empty($_POST["user_id"]))) {
+        $pgUserId = $_POST["user_id"];
+    } elseif((isset($_GET["user_id"])) && (!empty($_GET["user_id"]))) {
+        $pgUserId = $_GET["user_id"];
+    }
+
     if(Helper::is_post()){
         $api_token = Helper::post_val("api_token");
         if($api_token){
@@ -11,15 +18,15 @@
             $setting = $setting->where(["api_token" => $api_token])->one();
 
             if(!empty($setting)){
-               if(isset($_POST["user_id"])){
+               if(isset($pgUserId)){
                     $user = new User();                
-                        $user = $user->where(["id" => $_POST["user_id"]])->one();
+                        $user = $user->where(["id" => $pgUserId])->one();
                         if(!empty($user)){
                             if((isset($user->otp)) && (!empty($user->otp))){
                                 if($user->otp == $_POST['userOtp']){
                                     $updated = new User();
                                     $updated->status = 1;
-                                    $updated->id = $_POST['user_id'];
+                                    $updated->id = $pgUserId;
                                     if($updated->where(["id"=>$updated->id])->update()) $response->create(200, "User Mobile Number Verified Successfully", $updated->to_valid_array());
                                     else $response->create(201, "Something Went Wrong. Try Again.", null);
                                 }else $response->create(201, "Invalid Otp", null);
